99ac5037d19f493b0fb32ef6f99f89892b327fbb
Chima Anason <tblynxx@gmail.com>
2022-11-19 02:12:40 +0800

    Customer Model Controller and Service

---
+++ customer/src/main/java/com/anagracetech/customer/CustomerController.java
@@ -0,0 +1,17 @@
+package com.anagracetech.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/customers")
+public record CustomerController(CustomerService customerService) {
+
+    public void registerCustomer(@RequestBody CustomerRegisterationRequest customerRegisterationRequest){
+        log.info("new customer registration {}", customerRegisterationRequest);
+        customerService.registerCustomer(customerRegisterationRequest);
+    }
+}
---
+++ customer/src/main/java/com/anagracetech/customer/CustomerRegisterationRequest.java
@@ -0,0 +1,7 @@
+package com.anagracetech.customer;
+
+public record CustomerRegisterationRequest(
+        String firstName,
+        String lastName,
+        String email) {
+}
