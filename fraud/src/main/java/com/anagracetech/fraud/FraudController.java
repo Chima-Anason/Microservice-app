a210dc426eaf35fa36097ea77d4622252fac583f
Chima Anason <tblynxx@gmail.com>
2022-11-21 01:16:52 +0800

    Testing Microservice Communication   Closes #6.

--- fraud/src/main/java/com/anagracetech/fraud/FraudController.java
+++ fraud/src/main/java/com/anagracetech/fraud/FraudController.java
@@ -1,22 +1,22 @@
 package com.anagracetech.fraud;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
 
 
-    @PostMapping(path = "{customerId}")
+    @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
         boolean isFraudlentCustomer = fraudCheckService.isFraudlentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudlentCustomer);
     }
 }
