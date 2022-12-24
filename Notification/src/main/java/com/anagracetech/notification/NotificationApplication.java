37b41a62c391f067a650c13f0d4f2c70d56cdf99
Chima Anason <tblynxx@gmail.com>
2022-12-24 23:24:15 +0800

    Advance Message Queue Protocol configuration (RabbitMQ)  Closes #18, Closes #19, Closes #20, Closes #21, Closes #22, Closes #23, Closes #24 .

--- Notification/src/main/java/com/anagracetech/notification/NotificationApplication.java
+++ Notification/src/main/java/com/anagracetech/notification/NotificationApplication.java
@@ -1,15 +1,37 @@
 package com.anagracetech.notification;
 
+import com.anagracetech.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.anagracetech.notification",
+                "com.anagracetech.amqp"
+        }
+)
 @EnableDiscoveryClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+                                        NotificationConfig notificationConfig){
+        return args -> {
+            producer.publish(new Person("Ali", 32),
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+
+        };
+    }
+
+    record Person(String name, int age){}
 }
